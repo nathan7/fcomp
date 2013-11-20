ecf9a1c377e1eac5090d428a7dc3f34a35c6971c
Nathan Zadoks <nathan@nathan7.eu>
2013-11-20 22:41:45 +0100

    inline everything instead of doing fanciness

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 var apply = Function.prototype.apply
 module.exports = fcomp
 
-function fcomp() {
-  var fns = arguments
-    , len = fns.length
-    , fn = function() {
-        var val = apply.call(fns[0], null, arguments)
-        for (var i = 1; i < len; i++)
-          val = fns[i](val)
-        return val
-      }
-  for (var i = 0; i < len; i++)
-    fn.displayName = (fn.displayName || '')
-      + (i === 0 ? '' : ' · ')
-      + fns[i].name
+function fcomp(f, g, h, i, j) {
+  var len = arguments.length
+  if (len === 0)
+    return function(x) { return x }
+  if (len === 1)
+    return function() { return f.apply(this, arguments) }
+  if (len === 2)
+    return function() { return g.call(this, f.apply(this, arguments)) }
+  if (len === 3)
+    return function() { return h.call(this, g.call(this, f.apply(this, arguments))) }
+  if (len === 4)
+    return function() { return i.call(this, h.call(this, g.call(this, f.apply(this, arguments)))) }
+  if (len === 5)
+    return function() { return j.call(this, i.call(this, h.call(this, g.call(this, f.apply(this, arguments))))) }
+  var fn = fcomp(f, g, h, i, j)
+    , fns = [].slice.call(arguments, 5)
+  while (fns.length) {
+    fn = fcomp.apply(null, [fn].concat(fns.slice(0, 4)))
+    fns = [].slice.call(fns, 4)
+  }
   return fn
 }
 
